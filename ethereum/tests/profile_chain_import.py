16c37bea4fb9a0cda93e0017fabd29f271ae6ff3
Heiko Heiko <heiko@heiko.org>
2015-04-15 10:02:59 +0200

    more profiling

--- ethereum/tests/profile_chain_import.py
+++ ethereum/tests/profile_chain_import.py
@@ -1,12 +1,46 @@
 import os
 import rlp
+import time
 from ethereum.chain import Chain
 from ethereum.blocks import Block
 import ethereum.db as db
+from ethereum.utils import sha3_call_counter, sha3
+import cProfile
+import pstats
+import StringIO
+
+from ethereum.slogging import get_logger, configure_logging, get_configuration
+logger = get_logger()
+import sys
 from ethereum.slogging import configure
-configure(':trace')
+configure(':info')
 
 fn = os.path.join(os.path.dirname(__file__), 'blocks.fromthewire.hex.rlp')
+
+
+def iter_blocks(num=100):
+    longest = 0
+    i = 0
+    for rlp_data in open(fn):
+        for block_data in rlp.decode_lazy(rlp_data.strip().decode('hex')):
+            i += 1
+            if i == num:
+                break
+        if i == num:
+            break
+    print i, 'blocks scanned'
+
+
+def decode_blocks(num=100):
+    blocks = []
+    for rlp_data in open(fn):
+        blks = rlp.decode(rlp_data.strip().decode('hex'))
+        assert len(blks[0]) == 3
+        blocks.append(blks)
+        if len(blocks) >= num:
+            break
+    print len(blocks), 'blocks scanned'
+    return blocks
 
 
 def import_blocks(num=10):
@@ -34,5 +68,26 @@ def import_blocks(num=10):
             break
 
 
+def run():
+    # import_blocks(100)
+    # iter_blocks(1000000)
+    return decode_blocks(100)
+
 if __name__ == '__main__':
-    import_blocks(100)
+    if len(sys.argv) == 2:
+        pr = cProfile.Profile()
+        blocks = run()
+        pr.enable()
+        rlp.encode(blocks)
+        pr.disable()
+        s = StringIO.StringIO()
+        sortby = 'tottime'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats(50)
+        print s.getvalue()
+    else:
+        st = time.time()
+        run()
+        print
+        print 'took total', time.time() - st
+        print 'took w/o sha3', time.time() - st - sha3_call_counter[3]
